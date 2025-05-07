e25dd251c1b11f9c27e317c94528fcd32502d788
Gabriela Lins <infogabrielapitanga@gmail.com>
2025-05-07 12:35:45 +0100

    Completed Rails Yelp MVP challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,5 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Restaurant.create(name: "Epicure", address: "75008 Paris", phone_number: "0123456789", category: "french")
+Restaurant.create(name: "Sushi Zen", address: "Tokyo, Japan", phone_number: "0987654321", category: "japanese")
+Restaurant.create(name: "Pasta Palace", address: "Rome, Italy", phone_number: "0112233445", category: "italian")
+Restaurant.create(name: "Waffle Heaven", address: "Brussels, Belgium", phone_number: "0223344556", category: "belgian")
+Restaurant.create(name: "Dim Sum House", address: "Beijing, China", phone_number: "0334455667", category: "chinese")
